d34f6936cadf006e2d6b4ffca43c6ba0faf1d405
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-08-24 22:13:40 +0500

    To make a few changes in JS file

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var moreLink = document.getElementById("showMoreLink");
 var sidebarActivity = document.getElementById("sidebarActivity");
 
 function toggleMenu() {
-    profileMenu.classList.toggle("open-menu");
+  profileMenu.classList.toggle("open-menu");
 }
 
 function toggleActivity() {
-    sidebarActivity.classList.toggle("open-activity");
-    if(sidebarActivity.classList.contains("open-activity")){
-        moreLink.innerHTML = "Show less <b>-</b>";
-    }else {
-        moreLink.innerHTML = "Show More <b>+</b>";
-    }
+  sidebarActivity.classList.toggle("open-activity");
+  if (sidebarActivity.classList.contains("open-activity")) {
+    moreLink.innerHTML = "Show less <b>-</b>";
+  } else {
+    moreLink.innerHTML = "Show More <b>+</b>";
+  }
 }
